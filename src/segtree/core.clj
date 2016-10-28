e83e4719dbde81fe0e77b9ee42611002c75d51e0
caio <caioaao@gmail.com>
2016-10-27 23:29:14 -0200

    Implemented range-update with lazy propagation + fixed tests

--- src/segtree/core.clj
+++ src/segtree/core.clj
@@ -2,20 +2,47 @@
 
 (defmulti join-vals (fn [_ _ type] type))
 (defmulti identity-val (fn [tree] (:type tree)))
+(defmulti join-range-val (fn [tree _] (:type tree)))
 
-(defn join-trees [left right]
-  {:type      (:type left)
-   :value     (join-vals (:value left) (:value right) (:type left))
-   :left      left
-   :right     right
-   :idx-left  (:idx-left left)
-   :idx-right (:idx-right right)})
+(defmethod join-range-val :default [_ _] nil)
 
-(defn leaf [value idx tree-type]
-  {:type        tree-type
-   :value       value
+
+
+(defn- join-trees [left right]
+  (when (and left right)
+    {:type      (:type left)
+     :value     (join-vals (:value left) (:value right) (:type left))
+     :left      left
+     :right     right
+     :idx-left  (:idx-left left)
+     :idx-right (:idx-right right)}))
+
+(defn- leaf [value idx tree-type]
+  {:type      tree-type
+   :value     value
    :idx-left  idx
    :idx-right idx})
+
+(defn tap [x] (println x) x)
+
+(defn- imput-lazy-val [tree]
+  (assoc tree :lazy-val (or (:lazy-val tree) (identity-val tree))))
+
+(defn- reset-lazy-val [tree]
+  (assoc tree :lazy-val (identity-val tree)))
+
+(defn- relax-tree
+  [{:keys [left right lazy-val type] :as tree}]
+  (if lazy-val
+    (when-let [tree' (some-> tree (join-range-val lazy-val) reset-lazy-val)]
+      (if left
+        (merge tree'
+               {:left (-> (imput-lazy-val left)
+                          (update :lazy-val join-vals lazy-val type))
+                :right (-> (imput-lazy-val right)
+                          (update :lazy-val join-vals lazy-val type))})
+        tree'))
+    tree))
 
 (defn build
   ([arr tree-type]
@@ -29,32 +56,52 @@
 (defn query
   "Returns result of joining all values between `a` and `b`."
   [tree a b]
-  (cond
-    (or (< (:idx-right tree) a)
-        (> (:idx-left tree) b))
-    (identity-val tree)
+  (let [tree (or (relax-tree tree) tree)]
+    (cond
+      (or (< (:idx-right tree) a)
+          (> (:idx-left tree) b))
+      (identity-val tree)
 
-    (and (>= (:idx-left tree) a)
-         (<= (:idx-right tree) b))
-    (:value tree)
+      (and (>= (:idx-left tree) a)
+           (<= (:idx-right tree) b))
+      (:value tree)
 
-    :true
-    (join-vals (query (:left tree) a b)
-               (query (:right tree) a b)
-               (:type tree))))
+      :true
+      (join-vals (query (:left tree) a b)
+                 (query (:right tree) a b)
+                 (:type tree)))))
 
 (defn point-update
   "Joins `v` with value in `idx`."
   [tree idx v]
-  (cond
-    (or (< (:idx-right tree) idx)
-        (> (:idx-left tree) idx))
-    tree
+  (let [tree (or (relax-tree tree) tree)]
+    (cond
+      (or (< (:idx-right tree) idx)
+          (> (:idx-left tree) idx))
+      tree
 
-    (and (= (:idx-left tree) idx)
-         (= (:idx-right tree) idx))
-    (update tree :value join-vals v (:type tree))
+      (and (= (:idx-left tree) idx)
+           (= (:idx-right tree) idx))
+      (update tree :value join-vals v (:type tree))
 
-    :true
-    (join-trees (point-update (:left tree) idx v)
-                (point-update (:right tree) idx v))))
+      :true
+      (join-trees (point-update (:left tree) idx v)
+                  (point-update (:right tree) idx v)))))
+
+(defn range-update
+  "Joins `v` with all values between `a` and `b`. This function requires the
+  `join-range-val` method to be defined for the type of tree."
+  [tree a b v]
+  (when-let [tree (relax-tree tree)]
+    (cond
+      (or (< (:idx-right tree) a)
+          (> (:idx-left tree) b))
+      tree
+
+      (and (>= (:idx-left tree) a)
+           (<= (:idx-right tree) b))
+      (relax-tree (assoc tree :lazy-val v))
+
+      :true
+      (join-trees (range-update (:left tree) a b v)
+                  (range-update (:right tree) a b v)))))
--- test/segtree/core_test.clj
+++ test/segtree/core_test.clj
@@ -4,8 +4,10 @@
 
 
 (defmethod join-vals :sum [a b _] (+ a b))
-
 (defmethod identity-val :sum [_] 0)
+(defmethod join-range-val :sum
+  [{:keys [idx-left idx-right] :as tree} v]
+  (update tree :value join-vals (* (inc (- idx-right idx-left)) v) :sum))
 
 (deftest query-sum-test
   (let [arr [1 2 3 4 5 6 7 8 9 10]
@@ -17,19 +19,39 @@
         3 9
         2 6))
     (testing "Out of bounds queries return identity val"
-      (is (query st 15 20) (identity-val st)))))
+      (is (= (query st 15 20)
+             (identity-val st))))))
 
 
 (defn tree->arr [tree]
-  (for [i (range (:idx-left tree) (:idx-right tree))]
+  (for [i (range (:idx-left tree) (inc (:idx-right tree)))]
     (query tree i i)))
 
 (deftest point-update-test
   (let [arr [1 2 3 4 5 6 7 8 9 10]
         st (build arr :sum)]
     (testing "point updates behave properly"
-      (is (-> st (point-update 3 4) tree->arr)) [1 2 3 8 5 6 7 8 9 10]
-      (is (-> st (point-update 3 2) (point-update 0 4) (point-update 9 1) tree->arr)) [5 2 3 6 5 6 7 8 9 11])
+      (is (= (-> st (point-update 3 4) tree->arr)
+             [1 2 3 8 5 6 7 8 9 10]))
+      (is (= (-> st (point-update 3 2) (point-update 0 4) (point-update 9 1) tree->arr)
+             [5 2 3 6 5 6 7 8 9 11])))
 
     (testing "updates outside tree range keeps tree unmodified"
-      (is (tree->arr (point-update st 10 1)) [1 2 3 4 5 6 7 8 9 10]))))
+      (is (= (tree->arr (point-update st 10 1))
+             [1 2 3 4 5 6 7 8 9 10])))))
+
+(defmethod join-vals :sum-no-range-update [a b _] (+ a b))
+(defmethod identity-val :sum-no-range-update [_] 0)
+
+
+(deftest range-update-test
+  (let [arr [1 2 3 4]
+        st-no-range-update (build arr :sum-no-range-update)
+        st (build arr :sum)]
+    (testing "returns `nil` if `join-range-val` is not defined for the tree type"
+      (is (= (range-update st-no-range-update 0 3 1)
+             nil)))
+
+    (testing "updates range accordingly"
+      (is (= (-> st (range-update 0 3 1) tree->arr)
+             [2 3 4 5])))))
